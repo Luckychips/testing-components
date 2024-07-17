5775ec9391adf15f598a8accc312e95bda85d29a
todd.kim <sksms20000@gmail.com>
2024-07-18 03:24:08 +0900

    test code splitting.

--- vite.config.ts
+++ vite.config.ts
@@ -17,16 +17,16 @@ export default defineConfig({
     server: {
         port: 3000,
     },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: (id) => {
-                    if (id.indexOf('node_modules') !== -1) {
-                        const module = id.split('node_modules/').pop()?.split('/')[0];
-                        return `vendor-${module}`;
-                    }
-                },
-            },
-        },
-    },
+    // build: {
+    //     rollupOptions: {
+    //         output: {
+    //             manualChunks: (id) => {
+    //                 if (id.indexOf('node_modules') !== -1) {
+    //                     const module = id.split('node_modules/').pop()?.split('/')[0];
+    //                     return `vendor-${module}`;
+    //                 }
+    //             },
+    //         },
+    //     },
+    // },
 });
